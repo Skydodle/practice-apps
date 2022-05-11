d1be56992a8e981df493373a61bc09e2a4aa9c6b
skydodle <skydodle@gmail.com>
2022-05-11 17:13:38 -0500

    initial npm setups; .evn db.js set; server structured

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,4 +1,26 @@
 const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/glossary', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+let wordSchema = new mongoose.Schema({
+  word: {type: String, unique: true},
+  definition: String
+})
+
+let Entry = mongoose.model('Entry', wordSchema);
+
+let save = (entry) => {
+  let newEntry = new Entry({
+    word: entry.word,
+    definition: entry.definitioin
+  }).save()
+}
+module.exports.save = save;
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const db = require('./db.js')
 
 const app = express();
+
+// Parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+// GET method route
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage')
+})
+
+// POST method route
+app.post('/', (req, res) => {
+  res.send('hello world')
+})
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
