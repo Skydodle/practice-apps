495fc43df17f280d08c5effc0dd21114a56e3d08
skydodle <skydodle@gmail.com>
2022-05-11 23:38:56 -0500

    added addEntry button, tested entries found in mongosh ^______^ V

--- 1-glossary/client/src/components/Add.jsx
+++ 1-glossary/client/src/components/Add.jsx
@@ -2,26 +2,31 @@ import React from 'react';
 
 const Add = ({word, definition, handleInput, addEntry}) => {
   return(
-    <form>
-      <label>
-        Word
-        <input
-          type="text"
-          name="word"
-          value={word}
-          onChange={handleInput}
-        />
-      </label>
-      <label>
-        Definition
-        <input
-          type="text"
-          name="definition"
-          value={definition}
-          onChange={handleInput}
-        />
-      </label>
-    </form>
+    <div>
+      <h4>ADD MORE WORDS</h4>
+      <form>
+        <label>
+          Word
+          <input
+            type="text"
+            name="word"
+            value={word}
+            onChange={handleInput}
+          />
+        </label>
+        <label>
+          Definition
+          <input
+            type="text"
+            name="definition"
+            value={definition}
+            onChange={handleInput}
+          />
+        </label>
+      </form>
+      <button onClick={addEntry}>Add to glossary</button>
+    </div>
+
   )
 };
 
